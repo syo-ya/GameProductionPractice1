40455a6a83ae891981de2335d5963b3c10e6503a
Shingo Endo <shingo20050302@au.com>
2026-02-13 10:47:36 +0900

    GameSetting.h編集

---
+++ ゲーム制作課題「Breaking_UP」/Project/Source/GameSetting/GameSetting.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define SCREEN_WIDTH (1600)
+#define SCREEN_HEIGHT (900)
+#define SCREEN_COLOR_DEPTH (32)
+
+#define TRANS_COLOR_R (1)
+#define TRANS_COLOR_G (100)
+#define TRANS_COLOR_B (2)
