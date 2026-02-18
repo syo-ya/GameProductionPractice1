882507050bd5abb0f628ff0000dec066b7aa252a
HarukaUtunomiya <harukahmy@gmail.com>
2026-02-18 11:03:51 +0900

    a

--- ゲーム制作課題「Breaking_UP」/MapSource/MapChip.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "MapParameter.h"
-
-void LoadMapChipData(int stage);
-void CreateMap();
-void ResetMap();
-MapChipData GetMapChipData(int x, int y);
--- ゲーム制作課題「Breaking_UP」/MapSource/MapManager.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void InitMap();
-void LoadMap(int stage);
-void StartMap();
-void StepMap();
-void UpdateMap();
-void DrawMap();
-void FinMap();
